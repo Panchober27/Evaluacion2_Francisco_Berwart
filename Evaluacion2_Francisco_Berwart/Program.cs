461cf9133999ecf4b1f8b663b8bbb465c7a8651f
Francisco Berwart <58098908+Panchober27@users.noreply.github.com>
2022-06-13 21:06:41 -0400

    Agregar archivos de proyecto.

---
+++ Evaluacion2_Francisco_Berwart/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion2_Francisco_Berwart {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Prueba Qlia 2");
+            Console.ReadLine();
+
+        }
+    }
+}
